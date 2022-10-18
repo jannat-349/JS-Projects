c42a9d7aa1acab279534b16d5c8065c92932ac5a
jannat28-349 <jucse2.349@gmail.com>
2022-10-18 11:19:53 +0600

    ripple-button-commit

--- Multiplication-App/index.js
+++ Multiplication-App/index.js
@@ -4,8 +4,14 @@ const num2 = Math.ceil(Math.random()*10);
 const questionE1 = document.getElementById("question");
 const inputE1 = document.getElementById("input")
 const formE1 = document.getElementById("form");
+const scoreE1 = document.getElementById("score");
 
-let score = 0;
+
+let score = JSON.parse(localStorage.getItem("score"));
+if(!score){
+    score = 0;
+}
+scoreE1.innerText = `score: ${score}`;
 questionE1.innerText = `What is ${num1} multiply by ${num2} ?`;
 const correctAns = num1*num2;
 
@@ -13,11 +19,15 @@ formE1.addEventListener("submit", ()=>{
     const userAns = +inputE1.value;
     if(userAns === correctAns){
         score++;
-
+        updateLocalStorage();
     }else{
         score--;
+        updateLocalStorage();
     }
 })
 
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score));
+}
 
 
